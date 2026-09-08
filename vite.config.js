a2caf4ec7b3547282c77de360e2b45e73fa0a8e8
adithya0967 <adithyats0967@gmail.com>
2026-09-08 23:35:46 +0530

    local host link

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     port: 5173,
-    host: true
+    host: true,
+    open: true
   },
   test: {
     environment: 'node',
